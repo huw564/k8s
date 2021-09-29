a651ce1dee083f40866977c3d2696a64123c442c
ryanps1 <17645333+ryanps1@users.noreply.github.com>
2021-09-29 14:28:29 +0100

    Create deploy.tf

---
+++ infrastructure/test-rg/deploy.tf
@@ -0,0 +1,28 @@
+####################################
+############ TERRAFORM #############
+####################################
+
+provider "azurerm" { 
+  features {}
+}
+terraform {
+ backend "azurerm" {
+  storage_account_name = "#{terraformstorageaccount}#"
+  container_name       = "#{tfstatecontainer}#"
+  key                  = "#{tfstatename}#"
+  access_key  = "#{tf-storage-key}#"
+ }
+required_providers {
+  azurerm = {
+    source = "hashicorp/azurerm"
+    version = "2.20.0"
+  }
+}
+}
+
+
+#Resource Group
+resource "azurerm_resource_group" "ResourceGroup1" {
+  name     = "#{rgname1}#"
+  location = "West Europe"
+}
