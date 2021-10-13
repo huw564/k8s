0c83b5d3ee1717834169181b1fa76f4ad73edf0b
huwwill <91601437+huwwill@users.noreply.github.com>
2021-10-13 14:24:34 +0100

    Create outputs.tf

---
+++ modules/AzureRM/2.54.0/azurerm_sql_server/outputs.tf
@@ -0,0 +1,18 @@
+output "extended_auditing_policy" {
+  description = "returns a list of object"
+  value       = azurerm_sql_server.this.extended_auditing_policy
+}
+
+output "fully_qualified_domain_name" {
+  description = "returns a string"
+  value       = azurerm_sql_server.this.fully_qualified_domain_name
+}
+
+output "id" {
+  description = "returns a string"
+  value       = azurerm_sql_server.this.id
+}
+
+output "this" {
+  value = azurerm_sql_server.this
+}
