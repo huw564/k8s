ead5a5d691d8ceaea6f8afa4b7483eacccad0726
huwwill <91601437+huwwill@users.noreply.github.com>
2021-10-14 15:48:09 +0100

    added new acr

--- infrastructure/02_akspoc-acr-env-rg-001/deploy.tf
+++ infrastructure/02_akspoc-acr-env-rg-001/deploy.tf
@@ -1,1 +1,36 @@
-a
+####################################
+############ TERRAFORM #############
+####################################
+
+provider "azurerm" { 
+  features {}
+}
+terraform {
+ backend "azurerm" {
+  storage_account_name = "__terraformstorageaccount__"
+  container_name       = "__tfstatecontainer__"
+  key                  = "__tfstatename__"
+  access_key           = "__tf-storage-key__"
+ }
+required_providers {
+  azurerm = {
+    source = "hashicorp/azurerm"
+    version = "2.54.0"
+  }
+}
+}
+
+
+#Resource Group
+resource "azurerm_resource_group" "rg1" {
+  name     = "__rgname1__"
+  location = "West Europe"
+}
+
+#Container Registry
+module "acr1"{
+source = "../../modules/AzureRM/2.54.0/azurerm_container_registry"
+name                    = "__acr1_name__"
+location                = azurerm_resource_group.acr1.location
+resource_group_name     = azurerm_resource_group.acr1.name
+}
