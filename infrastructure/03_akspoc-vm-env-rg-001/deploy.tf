5ff02c26052b29d97f03e6c1865e00ae78add7f8
murphy10fs <78044599+murphy10fs@users.noreply.github.com>
2021-10-15 13:43:13 +0100

    Update deploy.tf

--- infrastructure/03_akspoc-vm-env-rg-001/deploy.tf
+++ infrastructure/03_akspoc-vm-env-rg-001/deploy.tf
@@ -1,1 +1,53 @@
+####################################
+############ TERRAFORM #############
+####################################
 
+provider "azurerm" { 
+  features {}
+}
+terraform {
+ backend "azurerm" {
+  storage_account_name = "__terraformstorageaccount__"
+  container_name       = "__tfstatecontainer__"
+  key                  = "__tfstatename__"
+  access_key           = "__tf-storage-key__"
+ }
+required_providers {
+  azurerm = {
+    source = "hashicorp/azurerm"
+    version = "2.54.0"
+  }
+}
+}
+
+
+#Resource Group
+resource "azurerm_resource_group" "rg1" {
+  name     = "__rgname1__"
+  location = "West Europe"
+}
+
+
+##Storage Account 1 ##
+module "StorageAccount1"{
+source = "../../modules/AzureRM/2.54.0/azurerm_storage_account"
+location     = azurerm_resource_group.rg1.location
+resource_group_name         = azurerm_resource_group.rg1.name
+name        = "__storage_account1_name__"
+account_replication_type    = "__storage_account1_replication_type__"
+  tags = {
+  "business unit" = "__tag_business_unit__",
+  "business owner" = "__tag_business_owner__",  
+  "business technical owner" = "__tag_business_technical_owner__",
+  "managed by" = "__tag_managed_by__",
+  "project" = "__tag_project__", 
+  "application" = "__tag_application__",   
+  "service importance" = "__tag_service_importance__", 
+  "cost centre" = "__tag_cost_centre__",
+  "deployment date" = "__tag_deployment_date__",  
+  "environment" = "__tag_environment__",
+  "resource group" = "__rgname1__",
+  "resource name" = "__storage_account1_name__",
+  "terraform deployed" = "__tag_terraformdeployed__"
+}
+}
