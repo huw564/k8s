8c870a444aa10b981601a39f1b9fccd613ed80da
huwwill <91601437+huwwill@users.noreply.github.com>
2021-10-20 11:35:21 +0100

    added basic acr to deploy

--- infrastructure/02_akspoc-acr-env-rg-001/deploy.tf
+++ infrastructure/02_akspoc-acr-env-rg-001/deploy.tf
@@ -27,3 +27,25 @@ resource "azurerm_resource_group" "rg1" {
   location = "West Europe"
 }
 
+##ACR1 ##
+module "acr1"{
+source = "../../modules/AzureRM/2.54.0/azurerm_container_registry"
+ name                  = "__acr1_name__"
+ location              = azurerm_resource_group.rg1.location
+ resource_group_name   = azurerm_resource_group.rg1.name
+  tags = {
+  "business unit" = "__tag_business_unit__",
+  "business owner" = "__tag_business_owner__",  
+  "business technical owner" = "__tag_business_technical_owner__",
+  "managed by" = "__tag_managed_by__",
+  "project" = "__tag_project__", 
+  "application" = "__tag_application__",   
+  "service importance" = "__tag_service_importance__", 
+  "cost centre" = "__tag_cost_centre__",
+  "deployment date" = "__tag_deployment_date__",  
+  "environment" = "__tag_environment__",
+  "resource group" = "__rgname1__",
+  "resource name" = "__acr1_name__",
+  "terraform deployed" = "__tag_terraformdeployed__"
+}
+}
