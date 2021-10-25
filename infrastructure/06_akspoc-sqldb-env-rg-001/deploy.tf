5226afbc373cc3e08144a7600f717ec0ec4d1ec6
ryanps1 <17645333+ryanps1@users.noreply.github.com>
2021-10-25 11:35:49 +0100

    Update deploy.tf

--- infrastructure/06_akspoc-sqldb-env-rg-001/deploy.tf
+++ infrastructure/06_akspoc-sqldb-env-rg-001/deploy.tf
@@ -56,3 +56,32 @@ administrator_login_password  = "__akspoc-sql-admin-login-password__"
   "terraform deployed" = "yes"
 }
 }
+
+### SQL database 1 ###
+module "SQLDB1"{
+source = "../../modules/AzureRM/2.54.0/azurerm_sql_database"
+name                                    = "__sql_database1_name__"
+resource_group_name                     = azurerm_resource_group.rg1.name
+location                                = azurerm_resource_group.rg1.location
+server_name                             = module.SQLServer1.name
+requested_service_objective_name        = "__sql_database1_performance__"
+collation                               = "__sql_database1_collation__"
+diagnostic_setting_name                 = "__sql_database1_diagnostic_setting_name__"
+diagnostic_setting_target_resource_id   = module.SQLDB1.id
+diagnostic_setting_existing_resource_id = "__shared-log-analytics-workspace-id-casesensitive__"
+  tags = {
+  "business unit" = "__tag_business_unit__",
+  "business owner" = "__tag_business_owner__",  
+  "business technical owner" = "__tag_business_technical_owner__",
+  "managed by" = "__tag_managed_by__",
+  "project" = "__tag_project__", 
+  "application" = "__tag_application__",   
+  "service importance" = "__tag_service_importance__", 
+  "cost centre" = "__tag_cost_centre__",
+  "deployment date" = "__tag_deployment_date__",  
+  "environment" = "__tag_environment__",
+  "resource group" = "__rgname1__",
+  "resource name" = "__sql_database1_name__",
+  "terraform deployed" = "yes"
+}
+}
