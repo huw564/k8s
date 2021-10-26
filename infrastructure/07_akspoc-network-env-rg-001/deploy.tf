fd1b30f8f66b6e76c3de1f84cdfb5b86f972fbaf
ryanps1 <17645333+ryanps1@users.noreply.github.com>
2021-10-26 13:42:14 +0100

    Update deploy.tf

--- infrastructure/07_akspoc-network-env-rg-001/deploy.tf
+++ infrastructure/07_akspoc-network-env-rg-001/deploy.tf
@@ -27,36 +27,6 @@ resource "azurerm_resource_group" "rg1" {
   location = "West Europe"
 }
 
-module "SQLServer1" {
-  source = "../../modules/AzureRM/2.54.0/azurerm_sql_server"
-name                          = "__sql_server1_name__" 
-resource_group_name           = azurerm_resource_group.rg1.name
-location                      = azurerm_resource_group.rg1.location
-administrator_login           = "__akspoc-sql-admin-login-username__" 
-administrator_login_password  = "__akspoc-sql-admin-login-password__"
-  identity = [{
-    principal_id = null
-    tenant_id    = null
-    type         = "SystemAssigned"
-  }]
-
-  tags = {
-  "business unit" = "__tag_business_unit__",
-  "business owner" = "__tag_business_owner__",  
-  "business technical owner" = "__tag_business_technical_owner__",
-  "managed by" = "__tag_managed_by__",
-  "project" = "__tag_project__", 
-  "application" = "__tag_application__",   
-  "service importance" = "__tag_service_importance__", 
-  "cost centre" = "__tag_cost_centre__",
-  "deployment date" = "__tag_deployment_date__",  
-  "environment" = "__tag_environment__",
-  "resource group" = "__rgname1__",
-  "resource name" = "__sql_server1_name__",
-  "terraform deployed" = "__tag_terraformdeployed__"
-}
-}
-
   
 module "azurerm_virtual_network" {
   source = "../../modules/AzureRM/2.54.0/azurerm_virtual_network"
