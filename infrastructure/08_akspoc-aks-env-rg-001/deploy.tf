944d7b2f0c8b8de1dcdae3db31139407b01fedd7
ryanps1 <17645333+ryanps1@users.noreply.github.com>
2021-10-26 14:04:29 +0100

    Update deploy.tf

--- infrastructure/08_akspoc-aks-env-rg-001/deploy.tf
+++ infrastructure/08_akspoc-aks-env-rg-001/deploy.tf
@@ -31,3 +31,77 @@ resource "azurerm_resource_group" "rg1" {
 module "azurerm_virtual_network" {
   source = "../../modules/AzureRM/2.54.0/azurerm_virtual_network"
 }
+
+  
+  
+module "azurerm_kubernetes_cluster" {
+  source = "../../../modules/AzureRM/2.54.0/azurerm_kubernetes_cluster"
+  dns_prefix = "devopstest"
+  location = "West Europe"
+  name = "DevOpsAKS001"
+  resource_group_name = azurerm_resource_group.RG.name
+
+  default_node_pool = [{
+    availability_zones           = []
+    enable_auto_scaling          = null
+    enable_host_encryption       = null
+    enable_node_public_ip        = null
+    max_count                    = null
+    max_pods                     = null
+    min_count                    = null
+    name                         = "k8spool"
+    node_count                   = "1"
+    node_labels                  = {}
+    node_taints                  = []
+    only_critical_addons_enabled = null
+    orchestrator_version         = null
+    os_disk_size_gb              = "30"
+    os_disk_type                 = null
+    proximity_placement_group_id = null
+    tags                         = {}
+    type                         = null
+    upgrade_settings = []
+    vm_size        = "Standard_D2_v2"
+    vnet_subnet_id = module.azurerm_subnet.this.id
+  }]
+  linux_profile = [{
+    admin_username = "ryan"
+    ssh_key = [{
+      key_data = "${file("//azp//agent//_work//_temp//publickeypem.pub")}"
+    }]
+  }]
+
+ network_profile = [{
+    dns_service_ip     = null
+    docker_bridge_cidr = null
+    load_balancer_profile = []
+    load_balancer_sku = null
+    network_mode      = null
+    network_plugin    = "azure"
+    network_policy    = null
+    outbound_type     = null
+    pod_cidr          = null
+    service_cidr      = null
+  }]
+  role_based_access_control = [{
+    azure_active_directory = [{
+      admin_group_object_ids = []
+      client_app_id          = null
+      server_app_id          = null
+      server_app_secret      = ""
+      tenant_id              = ""
+      managed                = true
+    }]
+    enabled = true
+  }]
+
+  service_principal = [{
+    client_id     = ""
+    client_secret = ""
+  }]
+
+  windows_profile = [{
+    admin_username = ""
+    admin_password = ""
+  }]
+}
