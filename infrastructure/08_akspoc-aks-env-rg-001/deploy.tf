ca18cab22c6be7d5b2484c576cced88afa796a53
ryanps1 <17645333+ryanps1@users.noreply.github.com>
2021-10-26 14:02:52 +0100

    Update deploy.tf

--- infrastructure/08_akspoc-aks-env-rg-001/deploy.tf
+++ infrastructure/08_akspoc-aks-env-rg-001/deploy.tf
@@ -1,1 +1,33 @@
+####################################
+############ TERRAFORM #############
+####################################
 
+provider "azurerm" { 
+  features {}
+}
+terraform {
+ backend "azurerm" {
+  storage_account_name = "__terraformstorageaccount__"
+  container_name       = "__tfstatecontainer__"
+  key                  = "__tfstatename__"
+  access_key           = "__tf-storage-key__"
+ }
+required_providers {
+  azurerm = {
+    source = "hashicorp/azurerm"
+    version = "2.54.0"
+  }
+}
+}
+
+
+#Resource Group
+resource "azurerm_resource_group" "rg1" {
+  name     = "__rgname1__"
+  location = "West Europe"
+}
+
+  
+module "azurerm_virtual_network" {
+  source = "../../modules/AzureRM/2.54.0/azurerm_virtual_network"
+}
