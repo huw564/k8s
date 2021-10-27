d357c9d88a884743d0901733d42e53842f4b4e3e
murphy10fs <78044599+murphy10fs@users.noreply.github.com>
2021-10-27 15:35:23 +0100

    Update deploy.tf

--- infrastructure/07_akspoc-network-env-rg-001/deploy.tf
+++ infrastructure/07_akspoc-network-env-rg-001/deploy.tf
@@ -76,3 +76,50 @@ module "subnet3" {
   resource_group_name = azurerm_resource_group.rg1.name
   virtual_network_name = module.azurerm_virtual_network.this.name
 }
+
+  
+  module "azurerm_private_dns_zone1" {
+  source = "../../modules/AzureRM/2.54.0/azurerm_private_dns_zone"
+  name = "__private_dns_name1__"
+  resource_group_name = azurerm_resource_group.rg1.name
+  tags = {  
+  "business unit" = "__tag_business_unit__",
+  "business owner" = "__tag_business_owner__",  
+  "business technical owner" = "__tag_business_technical_owner__",
+  "managed by" = "__tag_managed_by__",
+  "project" = "__tag_project__", 
+  "application" = "__tag_application__",   
+  "service importance" = "__tag_service_importance__", 
+  "cost centre" = "__tag_cost_centre__",
+  "deployment date" = "__tag_deployment_date__",  
+  "environment" = "__tag_environment__",
+  "resource group" = "__rgname1__",
+  "resource name" = "__private_dns_name1__",
+  "terraform deployed" = "__tag_terraformdeployed__"
+  }
+}
+    
+module "azurerm_private_dns_zone_virtual_network_link" {
+  source = "../../modules/AzureRM/2.54.0/azurerm_private_dns_zone_virtual_network_link"
+  name = "__private_dns_zone_virtual_network_link1__"
+  # private_dns_zone_name - (required) is a type of string
+  private_dns_zone_name = module.azurerm_private_dns_zone1.name
+  resource_group_name = azurerm_resource_group.rg1.name
+  # tags - (optional) is a type of map of string
+  tags = {
+    "business unit" = "__tag_business_unit__",
+  "business owner" = "__tag_business_owner__",  
+  "business technical owner" = "__tag_business_technical_owner__",
+  "managed by" = "__tag_managed_by__",
+  "project" = "__tag_project__", 
+  "application" = "__tag_application__",   
+  "service importance" = "__tag_service_importance__", 
+  "cost centre" = "__tag_cost_centre__",
+  "deployment date" = "__tag_deployment_date__",  
+  "environment" = "__tag_environment__",
+  "resource group" = "__rgname1__",
+  "resource name" = "__private_dns_zone_virtual_network_link1__",
+  "terraform deployed" = "__tag_terraformdeployed__"}
+  # virtual_network_id - (required) is a type of string
+  virtual_network_id = module.azurerm_virtual_network.id
+}
