0745ecd68cab11d970a2648a373445d069a08d4d
ryanps1 <17645333+ryanps1@users.noreply.github.com>
2021-11-18 15:02:05 +0000

    Update deploy.tf

--- infrastructure/08_akspoc-aks-env-rg-001/deploy.tf
+++ infrastructure/08_akspoc-aks-env-rg-001/deploy.tf
@@ -27,7 +27,21 @@ resource "azurerm_resource_group" "rg1" {
   location = "West Europe"
 }
 
+##cluster identity
+module "user_assigned_identity1" {
+  source = "../../modules/AzureRM/2.54.0/azurerm_user_assigned_identity"
+  location = azurerm_resource_group.rg1.location
+  name = "__identity1_name__"
+  resource_group_name = azurerm_resource_group.rg1.nam
+}
 
+##agentpool identity
+module "user_assigned_identity2" {
+  source = "../../modules/AzureRM/2.54.0/azurerm_user_assigned_identity"
+  location = azurerm_resource_group.rg1.location
+  name = "__identity2_name__"
+  resource_group_name = azurerm_resource_group.rg1.nam
+}
 module "azurerm_kubernetes_cluster" {
   source = "../../modules/AzureRM/2.54.0/azurerm_kubernetes_cluster"
   dns_prefix = "__aks1_dns_prefix_name__"
