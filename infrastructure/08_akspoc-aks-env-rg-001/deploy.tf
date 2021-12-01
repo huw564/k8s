c1b1ef2033a881d07824385b09ac419e62e7e24d
ryanps1 <17645333+ryanps1@users.noreply.github.com>
2021-12-01 13:45:02 +0000

    Update deploy.tf

--- infrastructure/08_akspoc-aks-env-rg-001/deploy.tf
+++ infrastructure/08_akspoc-aks-env-rg-001/deploy.tf
@@ -60,6 +60,15 @@ module "azurerm_role_assignment2" {
   # scope - (required) is a type of string
   scope = "__acr-rg-id__"
 }
+   
+   module "azurerm_role_assignment3" {
+  source = "../../modules/AzureRM/2.54.0/azurerm_role_assignment"
+  principal_id = module.user_assigned_identity2.principal_id
+  # role_definition_name - (optional) is a type of string
+  role_definition_name = "Owner"
+  # scope - (required) is a type of string
+  scope = "__network-rg-id__"
+}
     
     
 module "azurerm_kubernetes_cluster" {
